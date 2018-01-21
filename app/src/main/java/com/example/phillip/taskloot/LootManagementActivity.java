185d318d3311dd9c2f6bcace4da8a00c46d2d15e
Phillip Adamson <padamson@avantlink.com>
2018-01-21 15:05:19 -0700

    This is the initial commit.

---
+++ app/src/main/java/com/example/phillip/taskloot/LootManagementActivity.java
@@ -0,0 +1,13 @@
+package com.example.phillip.taskloot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LootManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loot_management);
+    }
+}
